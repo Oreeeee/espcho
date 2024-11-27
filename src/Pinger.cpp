bc4f6e007be5afff66ebc4fa0230f4a09425ed16
Oreeeee <oree3ee@gmail.com>
2024-11-27 18:40:13 +0100

    Add a toggle for Pinger logging

--- src/Pinger.cpp
+++ src/Pinger.cpp
@@ -1,6 +1,7 @@
 #include "BanchoState.h"
 #include "BanchoServer.h"
 #include "bancho/BanchoPackets.h"
+#include "config.h"
 #include <Arduino.h>
 
 void PingClient(void *arg) {
@@ -9,10 +10,16 @@ void PingClient(void *arg) {
         vTaskDelay(pdMS_TO_TICKS(10000)); // Wait for every 10 seconds to ping
         if (!bstate->alive) {
             // TODO: Pinger doesn't always end
+            #ifdef CHO_LOG_PINGER
             Serial.println("[PINGER] Quitting");
+            #endif
+
             vTaskDelete(NULL);
         }
+        #ifdef CHO_LOG_PINGER
         Serial.println("[PINGER] Pinging");
+        #endif
+        
         sendEmptyPacket(bstate, CHO_PACKET_PING);
     }
 }
